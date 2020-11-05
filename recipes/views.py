953902a51abcbbf7efd084e429408968db3aff98
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-05 15:45:45 -0800

    Add files via upload

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from django.template import loader
+
+from .models import Recipe
 
 def index(request):
-    return HttpResponse("My recipe book")
+    recipes_list = Recipe.objects.value()
+    template = loader.get_template('recipes/index.html')
+    context = {
+        'recipes_list': recipes_list,
+    }
+    return HttpResponse(template.render(context, request))
