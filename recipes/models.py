2c944210321c0c344250778b97cb89aeee571f82
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-04 17:20:57 -0800

    Add files via upload

---
+++ recipes/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    ingredients = models.TextField()
+    directions = models.TextField()
+    cook_time = models.IntegerField()
