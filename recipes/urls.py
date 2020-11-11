4e27d0b1bba98ec9328536030aaecfcd3926d4cb
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-10 20:53:28 -0800

    Add files via upload

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'recipes'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('create/', views.create, name='create'),
     path('detail/<recipe_id>/', views.detail, name='detail'),
     path('delete/<recipe_id>', views.delete, name='delete'),
