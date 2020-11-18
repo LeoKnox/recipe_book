17373fb04db0d927d5aef7791d5514a847ac8143
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-17 19:43:47 -0800

    Add files via upload

--- preparations/views.py
+++ preparations/views.py
@@ -5,6 +5,6 @@ from recipes.models import Recipe
 
 def index(request):
     prep_data = Preparation.objects.values()
-    recipe_data = Recipe.objects.value()
-    context = {'prep_data': prep_data, 'recipe_data': 'recipe_data'}
+    recipe_data = Recipe.objects.values()
+    context = {'prep_data': prep_data, 'recipe_data': recipe_data}
     return render(request, 'preparations/index.html', context)
