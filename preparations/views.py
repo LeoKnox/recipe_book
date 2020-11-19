26dd8b7c82ebdc490ad9def4f68380e5910cde6a
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-18 19:37:07 -0800

    Add files via upload

--- preparations/views.py
+++ preparations/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
-from .models import Preparation
+from .models import Preparation, Recipe_Prep
 from recipes.models import Recipe
 
 def index(request):
     prep_data = Preparation.objects.values()
     recipe_data = Recipe.objects.values()
-    context = {'prep_data': prep_data, 'recipe_data': recipe_data}
+    recipe_prep = Recipe_Prep.objects.get(recipe_id=1)
+    print(recipe_prep.prep_name)
+    context = {'prep_data': prep_data, 'recipe_data': recipe_data, 'recipe_prep': recipe_prep}
     return render(request, 'preparations/index.html', context)
